8ab1a2f56fe87b6a6efd192b3b91800fd6516a45
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-23 09:29:36 -0300

    Updates

---
+++ precache-manifest.28d4176142216a1d80cdbc9af2d8b7bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd3ed28f98f1570b89bfab08401d8cc",
+    "url": "/pokedex-test/index.html"
+  },
+  {
+    "revision": "3c4ec33fb6e2dea105c6",
+    "url": "/pokedex-test/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/pokedex-test/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pokedex-test/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4ec33fb6e2dea105c6",
+    "url": "/pokedex-test/static/js/main.be39f5ae.chunk.js"
+  },
+  {
+    "revision": "3acf312db673558298ed",
+    "url": "/pokedex-test/static/js/runtime-main.a60d3f4c.js"
+  }
+]);
